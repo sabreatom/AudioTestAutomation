fa16dea3031af4496d357838be349ed0abaa9ba2
Aleksandrs <aleksandrs.maklakovs@gmail.com>
2022-04-04 01:33:55 +0300

    Add AFR test uses AFR manager class

--- test_measureAFR.py
+++ test_measureAFR.py
@@ -1,23 +1,17 @@
 import src.WaveFormGenerator as wfg
 import src.AudioInterface as ai
 import src.DutOutputAnalyzer as doa
+import AfrTestManager as afr_tm
 
 import pytest
 import time
 
+meas_freq = [500, 1000, 1500, 2000, 4000, 10000]
+
 def test_afr():
-    def measureEndCallback(tone, amplitude, thd):
-        print("Callback: Tone value: {}, THD: {}, amplitude: {}".format(tone, thd, amplitude))
-        audioInterface.stop()
-
-    testInProgress = True
-    waveformGnerator = wfg.WaveFormGenerator(wfg.WaveFormType.Sine, 500, 44100, 1.0)
-    dutOutputAnalyzer = doa.DutOutputAnalyzer(44100, 500, measureEndCallback)
-    audioInterface = ai.AudioInterface(44100, dutOutputAnalyzer.processDutData, waveformGnerator.generateSamplesCallback)
-    audioInterface.start()
-
-    while audioInterface.isRunning():
-        time.sleep(0.1)
-
-    assert True
-
+    wfg_inst = wfg.WaveFormGenerator(wfg.WaveFormType.Sine, 50, 44100)
+    doa_inst = doa.DutOutputAnalyzer(44100, 50)
+    ai_inst = ai.AudioInterface(44100, doa_inst.processDutData, wfg_inst.generateSamplesCallback)
+    afr_tm_inst = afr_tm.AfrTestManager(ai_inst, wfg_inst, doa_inst, meas_freq)
+    doa_inst.measurement_done_callback = afr_tm_inst.callbackMeasurementDone
+    afr_tm_inst.runTestIteration()
