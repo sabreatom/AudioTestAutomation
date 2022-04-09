4f48a145b85aaa11a6a7544db168885bf45e94fa
Aleksandrs Maklakovs <aleksandrs.maklakovs@gmail.com>
2022-04-10 01:16:06 +0300

    Add thread safe frequency change

--- src/DutOutputAnalyzer.py
+++ src/DutOutputAnalyzer.py
@@ -4,30 +4,17 @@ from scipy.fftpack import fft
 import math
 
 class DutOutputAnalyzer:
-    def __init__(self, sample_rate, frequency):
+    def __init__(self, sample_rate, measurement_done_callback = NULL):
         self.sample_rate = sample_rate
-        self.frequency = frequency
         self.frequency_tolerance = 20
 
         self.buffer_size = sample_rate // 20 #at least one 20 Hz sine wave period fits in buffer
         self.buffer = np.zeros(self.buffer_size)
         self.buffer_sample_count = 0
 
-        self.measurement_done_callback = NULL
+        self.measurement_done_callback = measurement_done_callback
 
         self.last_thd = None
-
-    def setRefFrequency(self, frequency):
-        self.frequency = frequency
-
-    def getRefFrequency(self):
-        return self.frequency
-
-    def setRefAmplitude(self, amplitude):
-        self.ref_amplitude = amplitude
-
-    def getRefAmplitude(self):
-        return self.ref_amplitude
 
     def storeBuffer(self, data):
         audio_data = np.frombuffer(data, dtype=np.float32)
@@ -66,12 +53,7 @@ class DutOutputAnalyzer:
             result['max_frequency_amplitude'] = y_f[max_harmonic]
             result['max_frequency'] = self.convertHarmonicToFrequency(max_harmonic, len(y_f))
 
-            ref_harmonic = self.convertFrequencyToHarmonic(self.frequency, len(y_f))
-            harmonic_tolerance = self.convertFrequencyToHarmonic(self.frequency_tolerance, len(y_f))
-            ref_harmonic = np.argmax(y_f[ref_harmonic - harmonic_tolerance:ref_harmonic + harmonic_tolerance])
-            result['thd'] = self.calculateTHD(ref_harmonic, y_f[:self.buffer_size // 2])
-            result['ref_frequency_amplitude'] = y_f[ref_harmonic]
-            result['ref_frequency'] = self.convertHarmonicToFrequency(ref_harmonic, len(y_f))
+            result['thd'] = self.calculateTHD(max_harmonic, y_f[:self.buffer_size // 2])
             result['fft'] = y_f
 
             self.measurement_done_callback(result)
